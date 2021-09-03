700d1a68b914a7f7320650b5a145c89d1425de0a
Pruthvi Mohan <pruthvi.mohan@goavega.com>
2021-09-03 18:53:55 +0530

    .

--- NodeJS/Routes/ProductRoute.js
+++ NodeJS/Routes/ProductRoute.js
@@ -2,9 +2,7 @@
 const express = require('express')
 const router = express.Router()
 
-// const Products = require('../DataFile/ProductList')
-
-const Products = [{
+let Products = [{
     id:1,
     title:"Prod1",
     description:"Prod1 Description"
@@ -29,32 +27,31 @@ const Products = [{
 
 //GET method - retrives all data
 router.get("/",(req,res)=>{
-    res.json({Products:Products})
+    res.send({Products:Products})
     })
 
 
 //GET method - retrieves only one item
-router.get("/id",(req,res)=>{
-    res.json({Products:Products.id})
+router.get("/:id",(req,res)=>{
+    const id = req.params.id
+    for(let product of Products){
+        if(product.id === id){
+            res.json(product)
+            return
+        }
+    }
+    res.status(404).send("Record not found")
     })
 
 
 //POST method - Adds item to DB
-router.post("/", async (req,res) =>{
-    try
-    {
-        let{id,title,description} = req.body
-        const newProduct = new Product({
-            id,
-            title,
-            description
-        })
-        const saveProduct = await newProduct.save()
-        res.json(saveProduct)
-    }catch(err){
-        res.status(500).json({error:err.message})
-    }
+router.post("/",  (req,res) =>{
+   const product = req.body
+   console.log(product)
+   Products.push(product)
+   res.send("posted successfully")
 })
+
 
 //PUT Method - Update item
 router.put("/:id",(req,res)=>{
